157dd83680cd452f151690e4a04b0ee50162d08d
Brian Rhodes <brianr4548@gmail.com>
2020-09-23 23:57:02 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,10 +2,11 @@ def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
+  
 end
 
 def toadstool
-  puts status #wrong method
+   #wrong method
 end
 
 def link
